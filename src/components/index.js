96d183eee83335750d0afbd6f451ba62dac5ee2d
robinsonclo2 <rclower@cscc.edu>
2023-11-02 16:37:21 -0600

    Updates

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Home from './Home';
-import Portals from './Portals';
-import MontyHall from './MontyHall';
-import APISearch from './APISearch';
-import ReactAtom from './ReactAtom';
-import Toggle from './Toggle';
-import Interview from './Interview';
-
-export { Home, Portals, MontyHall, APISearch, ReactAtom, Toggle, Interview }
